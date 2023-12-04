4038f4f242073fef079f0ebfc0315a630a0f5537
DhrumilP <dhrumil172@gmail.com>
2023-12-03 23:01:00 -0500

    added team urls to team management urls

--- teamManagementApp/urls.py
+++ teamManagementApp/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+
+from team.views import TeamMemberListView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('team/', include('team.urls')),  # your team app URLs
+    path('', TeamMemberListView.as_view(), name='home'),  # set the root URL
 ]
+
